4b1c086baeac965e598a59a034f4e57db2d6b39f
JackPliskin64 <jackpliskin64@outlook.es>
2023-02-10 20:06:58 +0100

    Revert "Revert "Todo implementado correctamente""
    This reverts commit 388d79926d70aec54959364bf105afd760f0548d.

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -64,7 +64,6 @@ public class PlayerController : MonoBehaviour, ISavable
             var triggerable = collider.GetComponent<IPlayerTriggerable>();
             if (triggerable != null)
             {
-                character.Animator.isMoving = false;
                 triggerable.OnPlayerTriggered(this);
                 break;
             }
